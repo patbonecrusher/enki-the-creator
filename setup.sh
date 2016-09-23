a0d9f358e9ea0edf7036b288f738a80c6f1d8396
Patrick Laplante <laplante.patrick@gmail.com>
2016-09-23 16:46:25 -0400

    added scripts and config stuff

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+# -----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <bonecrusher@covenofchaos.com>  wrote this file.  As long as  you retain this
+# notice you can do whatever you want with this stuff.  If we meet some day and
+# you think this stuff is worth it, you can buy me a beer in return.
+#  > BoneCrusher <
+# -----------------------------------------------------------------------------
+
+mydir=${0:a:h}
+config_dir=${mydir}/../configs
+
+[[ $1 ]] && { config_dir = $1 }
+
+installers=(
+  ohmyzsh
+  brew
+  node
+  vscode
+  atom
+  vim
+  java
+  ruby
+  aspnet
+  powerlinefonts
+)
+
+for installer in "${installers[@]}"
+do
+  if [[ -f ${config_dir}/${installer} ]]
+  echo 'setting up' "${installer}"
+  ${mydir}/scripts/setup_${installer} ${config_dir}/${installer} 
+done
